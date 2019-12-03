a909f464d581d284842225a0ac29670b0af6c324
HowardChen <howard3108@hotmail.com>
2019-12-03 12:38:43 -0500

    Updates

---
+++ precache-manifest.e57d37cfb4a957d102dd1bc40e85cce7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2a296a401c55a69deeaf9544343ca1",
+    "url": "/howard-chen-project-five/index.html"
+  },
+  {
+    "revision": "8b06bcfba6904ed8d203",
+    "url": "/howard-chen-project-five/static/css/2.dfe7030c.chunk.css"
+  },
+  {
+    "revision": "6bf9fd11bc4b76635162",
+    "url": "/howard-chen-project-five/static/css/main.613271ab.chunk.css"
+  },
+  {
+    "revision": "8b06bcfba6904ed8d203",
+    "url": "/howard-chen-project-five/static/js/2.6d634b57.chunk.js"
+  },
+  {
+    "revision": "6bf9fd11bc4b76635162",
+    "url": "/howard-chen-project-five/static/js/main.d5d259c3.chunk.js"
+  },
+  {
+    "revision": "7769a81e554decc82315",
+    "url": "/howard-chen-project-five/static/js/runtime-main.747166f3.js"
+  },
+  {
+    "revision": "150b7a85bd4abe5b52d0a4513b128358",
+    "url": "/howard-chen-project-five/static/media/background.150b7a85.webp"
+  }
+]);
